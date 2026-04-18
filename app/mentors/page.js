0940212a1790f4edf6a794e2ab75fd041b4ab9ba
Abzal1214 <saylaubayvip007@gmail.com>
2026-04-18 11:50:17 +0500

    fix(debug): move debug block inside JSX tree

--- app/mentors/page.js
+++ app/mentors/page.js
@@ -67,12 +67,14 @@ export default function MentorsPage() {
   }, [user, mentors]);
 
   return (
-        {/* DEBUG: временный вывод для отладки */}
-        <div className="fixed bottom-0 left-0 bg-white/90 text-xs p-2 z-[9999] max-w-lg border border-rose-200 rounded">
-          <div><b>user:</b> {JSON.stringify(user)}</div>
-          <div><b>isAdmin:</b> {String(isAdmin)}</div>
-          <div><b>mentors:</b> {JSON.stringify(mentors)}</div>
-        </div>
+        <>
+          {/* ...основной JSX... */}
+          <div className="fixed bottom-0 left-0 bg-white/90 text-xs p-2 z-[9999] max-w-lg border border-rose-200 rounded">
+            <div><b>user:</b> {JSON.stringify(user)}</div>
+            <div><b>isAdmin:</b> {String(isAdmin)}</div>
+            <div><b>mentors:</b> {JSON.stringify(mentors)}</div>
+          </div>
+        </>
     <div className="min-h-screen">
       <div className="relative flex flex-col items-center justify-center text-center px-6 py-16">
         <div className="absolute inset-0 bg-black/50 backdrop-blur-[2px]" />
