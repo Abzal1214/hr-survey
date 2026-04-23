e6b10e9944f679cb4d8053d714512157b905dbeb
Abzal1214 <saylaubayvip007@gmail.com>
2026-04-23 10:56:44 +0500

    Testing

---
+++ test-mongo.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+const uri = 'mongodb+srv://Abzal:Hawaii2026@aquapark.dspgwyv.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(uri)
+  .then(() => { console.log('✅ Соединение с MongoDB установлено!'); process.exit(0); })
+  .catch(err => { console.error('❌ Ошибка соединения:', err.message); process.exit(1); });
