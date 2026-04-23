9a4211e9d14ff2959805fcd03751b3d4039fba95
Abzal1214 <saylaubayvip007@gmail.com>
2026-04-23 17:44:02 +0500

    junior

--- test-mongo.js
+++ test-mongo.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const uri = 'mongodb+srv://Abzal:Hawaii2026@aquapark.dspgwyv.mongodb.net/?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://Abzal:Hawaii2026@cluster0.hwwhnre.mongodb.net/?retryWrites=true&w=majority';
 
 mongoose.connect(uri)
   .then(() => { console.log('✅ Соединение с MongoDB установлено!'); process.exit(0); })
